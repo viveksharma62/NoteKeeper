aefb18c77ce1b9269755d8d40a3e092fa3c431af
VIVEK KUMAR <142143383+viveksharma62@users.noreply.github.com>
2025-12-27 14:55:54 +0530

    Update url.js

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const BACKEND_URL = axios.create({
-    baseURL: 'http://localhost:4001/api/vi1/notes',
-    // baseURL: 'https://notekeeper-1n1c.onrender.com/api/vi1/notes',
+    // baseURL: 'http://localhost:4001/api/vi1/notes',
+    baseURL: 'https://notekeeper-1k8b.onrender.com/api/vi1/notes',
 })
 
 export default BACKEND_URL;
